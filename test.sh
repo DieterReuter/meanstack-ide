fad7887618de5073f2e4931e8e02da2d31ecec95
Dieter Reuter <dieter.reuter@me.com>
2014-02-10 13:15:16 +0100

    Cleanup

--- test.sh
+++
@@ -1,8 +0,0 @@
-sudo npm install -g powerline
-sudo -E -u vagrant cat << 'BASHRC' >> /home/vagrant/.bashrc
-  function _update_ps1() {
-    export PS1="$(powerline $? --shell bash --depth 4)"
-  }
-BASHRC
-export PROMPT_COMMAND="_update_ps1"
-
